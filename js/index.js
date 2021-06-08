cc3c93fdad0e87d6a9031999ce1e269947d1df94
Denis-Pankov <81138569+Denis-Pankov@users.noreply.github.com>
2021-06-08 07:39:04 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -510,28 +510,28 @@ document.addEventListener("DOMContentLoaded", function () {
 // Отправка формы
 "use strict"
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
 
-  async function formSend(e) {
-    e.preventDefault();
+	async function formSend(e) {
+		e.preventDefault();
 
-    let error = formValidate(form);
-    
-    let formData = new FormData(form);
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
 
     if (error === 0) {
-      form.classList.add('_sending');
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData
-      });
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        form.reset();
-        form.classList.remove('_sending');
-      } else {
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
         alert("Ошибка!!! Что-то пошло не так(");
         form.classList.remove('_sending');
       }
--- sendmail.php
+++ sendmail.php
@@ -1,45 +1,44 @@
 <?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
-  require 'phpmailer/src/Exception.php';
-  require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
 
-  $mail = new PHPMailer(true);
-  $mail->CharSet = 'UTF-8';
-  $mail->setLanguage('ru', 'phpmailer/language/');
-  $mail->IsHTML(true);
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
 
   //От кого письмо
-  $mail->setFrom('denispankov11@gmail.com', 'Денис Панков');
+  $mail->setFrom('angelman777@mail.com', 'Денис Панков');
   //Кому отправить
-  $mail->addAddress('angelman@mail.ru');
+  $mail->addAddress('denispankov11@gmail.com');
   //Тема письма
-  $mail->Subject = "Привет! Магия PHP Работает!";
+  $mail->Subject = 'Привет! Магия PHP Работает!';
 
   //Тело письма
   $body = '<h1>Видишь это?! Ты - КРУТ!</h1>';
 
   if(trim(!empty($_POST['name']))){
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-  }
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
   if(trim(!empty($_POST['tel']))){
-    $body.='<p><strong>Номер:</strong> '.$_POST['tel'].'</p>';
-  }
+		$body.='<p><strong>Имя:</strong> '.$_POST['tel'].'</p>';
+	}
 
   $mail->Body = $body;
 
   //Отправка
   if (!$mail->send()) {
-    $message = 'Ошибка???';
-  } else {
-    $message = 'Данные отправлены!';
-  }
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!!!!';
+	}
 
-  $response = ['message' => $message];
+	$response = ['message' => $message];
 
-  header('Content-type: application/json');
-  echo json_encode($response);
-
+	header('Content-type: application/json');
+	echo json_encode($response);
 ?>
 
