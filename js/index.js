b4eddbf2c94b0a1b809965b3f094c67accc8eafc
Denis-Pankov <81138569+Denis-Pankov@users.noreply.github.com>
2021-06-09 06:45:57 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -508,79 +508,65 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Отправка формы
-"use strict"
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
 
-	async function formSend(e) {
-		e.preventDefault();
+let selector = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('+7 (999) 999-99-99');
+im.mask(selector);
 
-		let error = formValidate(form);
+let selector2 = document.querySelector('input[type="tel"]');
 
-		let formData = new FormData(form);
+selector2.addEventListener('input', function(){
+	console.log(selector2.value)
 
-    if (error === 0) {
-			form.classList.add('_sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				form.reset();
-				form.classList.remove('_sending');
-			} else {
-        alert("Ошибка!!! Что-то пошло не так(");
-        form.classList.remove('_sending');
-      }
-    } else {
-        alert('Заполните обязательные поля корректно, чтобы мы смогли дозвониться');
-    }
 
-  }
+	const re = /^\d*(\.\d+)?$/
 
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('._req');
+	console.log(selector2.value.match(/[0-9]/g).length)
 
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
 
-      if (input.classList.contains('_tel')){
-        if (telTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else {
-        if (input.value === '') {
-          formAddError(input);
-          error++;
-        }
-      }
-    }
-    return error;
-  }
-    function formAddError(input) {
-      input.parentElement.classList.add('_error');
-      input.classList.add('_error');
-    }
-    function formRemoveError(input) {
-      input.parentElement.classList.remove('_error');
-      input.classList.remove('_error');
-    }
-    function telTest(input) {
-      return !/^\d[\d\(\)\ -]{4,14}\d$/.test(input.value);
-    }
+	console.log(selector2.value.replace(/[0-9]/g, "0"));
+	
 });
 
+// const fileInput = document.querySelector('input[type="file"]');
 
+// fileInput.addEventListener('change', (e) => {
+// 	let files = e.currentTarget.files;
+// 	console.log(files);
 
+// 	if (files.length) {
+// 		fileInput.closest('label').querySelector('span').textContent = files[0].name;
+// 	} else {
+// 		fileInput.closest('label').querySelector('span').textContent = 'Прикрепить файл';
+// 	}
 
+// });
 
+let validateForms = function(selector, rules, successModal, yaGoal) {
+	new window.JustValidate(selector, {
+		rules: rules,
+		submitHandler: function(form) {
+			let formData = new FormData(form);
 
+			let xhr = new XMLHttpRequest();
 
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						console.log('Отправлено');
+					}
+				}
+			}
 
+			xhr.open('POST', 'mail.php', true);
+			xhr.send(formData);
+
+			form.reset();
+
+			// fileInput.closest('label').querySelector('span').textContent = 'Прикрепить файл';
+		}
+	});
+}
+
+validateForms('.form', { email: {required: true, email: true}, tel: {required: true} }, '.thanks-popup', 'send goal');
 
